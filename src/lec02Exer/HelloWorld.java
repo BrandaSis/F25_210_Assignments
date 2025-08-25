ee6aaa85349355a4c4fa377bebfc4cac89fb4c30
Branda Sisoutham <brandas@unc.edu>
2025-08-25 13:48:47 -0400

    1

---
+++ src/lec02Exer/HelloWorld.java
@@ -0,0 +1,23 @@
+package lec02Exer;
+
+// Option A
+public class HelloWorld {
+    public static void main(String[] args1){
+        System.out.println("Hello; World!");
+    }
+}
+
+
+// Option B
+//public class HelloWorld {
+//    public static void main(string[] args){
+//        System.out.println("Hello World!");
+//    }
+//}
+
+// Option C
+//public class HelloWorld {
+//    public static void main(String[] args){
+//        System.out.println("Hello World!")
+//    }
+//}
